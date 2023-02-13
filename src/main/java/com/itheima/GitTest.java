4ef51076da2929be7c956f9aa279449e76a88039
Hyuaneee <81038583@qq.com>
2023-02-13 14:37:26 +0800

    master commit2

--- src/main/java/com/itheima/GitTest.java
+++ src/main/java/com/itheima/GitTest.java
@@ -3,6 +3,6 @@ package com.itheima;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
-        System.out.println("hello git hot-fix1");
+        System.out.println("hello git master");
     }
 }
