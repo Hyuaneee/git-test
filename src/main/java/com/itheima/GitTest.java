0fc6a56d26aa9673ca82f4316a9a31d79f1cbdaf
Hyuaneee <81038583@qq.com>
2023-02-13 14:04:30 +0800

    hot-fix commit1

--- src/main/java/com/itheima/GitTest.java
+++ src/main/java/com/itheima/GitTest.java
@@ -2,7 +2,7 @@ package com.itheima;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hellogit");
-        System.out.println("abc");
+        System.out.println("hello git");
+        System.out.println("hello git hot-fix1");
     }
 }
