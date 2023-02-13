81440b0111f24fb6904fe5b0466ab798c835e429
Yuan <125137703+Hyuaneee@users.noreply.github.com>
2023-02-13 15:11:28 +0800

    pull commit1

--- src/main/java/com/itheima/GitTest.java
+++ src/main/java/com/itheima/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git master");
         System.out.println("hello git hot-fix2");
         System.out.println("push 1");
+        System.out.println("pull 1");
     }
 }
